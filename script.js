87cf2b897353f54fd2323ad6dd7b12811ebdca71
Nabarun <nicknabarun@gmail.com>
2020-11-06 13:56:58 +0530

    Document the code

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 let blackjackGame = {
+  /*
+  This is an Object.
+
+  What's happenning here is k-v pairs are being created in a way such that the keys
+  represent a code word for a specific element, making it easier to access them.
+  */
+
   'you': {'scoreSpan': '#your-blackjack-result', 'div': '#your-box', 'score': 0},
   'dealer': {'scoreSpan': '#dealer-blackjack-result', 'div': '#dealer-box', 'score': 0},
-  'cards': ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'K', 'J', 'Q', 'A'],
-  'cardsMap': {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'K': 10, 'J': 10, 'Q': 10, 'A': [1, 11]},
+  'cards': ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'K', 'J', 'Q', 'A'],  // This array is used to generate random cards
+  'cardsMap': {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'K': 10, 'J': 10, 'Q': 10, 'A': [1, 11]},  // These k-v pairs help in mapping integers to the cards so that it's possible to keep a count of the score 
   'wins': 0,
   'losses': 0,
   'draws': 0,
-  'isStand': false,
-  'turnsOver': false,
+  'isStand': false,   // Keeps track of whether the 'Stand' button has been pressed
+  'turnsOver': false, // Keeps track of all the turns; meaning - to check whether 'Hit' and 'Deal' buttons have been pressed
 };
 
+// The below two constants help in easier retrieval of keys
 const YOU = blackjackGame['you'];
 const DEALER = blackjackGame['dealer'];
 
@@ -17,14 +25,23 @@ const hitSound = new Audio('sounds/swish.m4a');
 const winSound = new Audio('sounds/cash.mp3');
 const lossSound = new Audio('sounds/aww.mp3');
 
+/*
+'querySelector' is better than 'getElementById' and it is more convenient
+ 
+'addEventlistener' determines what happens after a particular event occurs
+and it is more convenient than using attributes like 'onclick' in HTML
+*/
+
 document.querySelector('#blackjack-hit-button').addEventListener('click', blackjackHit);
 
 document.querySelector('#blackjack-stand-button').addEventListener('click', dealerLogic);
 
 document.querySelector('#blackjack-deal-button').addEventListener('click', blackjackDeal);
 
+// This function determines what happens when 'Hit' button is pressed
 function blackjackHit() {
   if (blackjackGame['isStand'] === false) {
+    //This code works if the 'Stand' button has not been pressed
     let card = randomCard();
     showCard(card, YOU);
     updateScore(card, YOU);
@@ -33,34 +50,44 @@ function blackjackHit() {
 }
 
 function randomCard() {
-  let randomIndex = Math.floor(Math.random() * 13);
+  let randomIndex = Math.floor(Math.random() * 13);  // Multiplied by 13 since there are 13 possibilities
   return blackjackGame['cards'][randomIndex];
 }
 
 function showCard(card, activePlayer) {
   if (activePlayer['score'] <= 21) {
+    // Show cards only if the total score is less than or equal to 21
     let cardImage = document.createElement('img');
-    cardImage.src = `images/${card}.png`;
+    cardImage.src = `images/${card}.png`;  // String template
     document.querySelector(activePlayer['div']).appendChild(cardImage);
     hitSound.play();
   }
 }
 
+// This function determines what happens when 'Deal' button is pressed
 function blackjackDeal() {
   if (blackjackGame['turnsOver'] === true) {
+    /*
+    'Deal' button can only be pressed when all the turns are over.
+    And after pressing the 'Deal' button, we should be able to press
+    the 'Stand' button again; hence 'isStand' is set to false.
+    */
     blackjackGame['isStand'] = false;
     
-    let yourImages = document.querySelector('#your-box').querySelectorAll('img'); //It's an Array
-    let dealerImages = document.querySelector('#dealer-box').querySelectorAll('img'); //It's an Array
+    let yourImages = document.querySelector('#your-box').querySelectorAll('img'); //It's an Array of all the images inside '#your-box'
+    let dealerImages = document.querySelector('#dealer-box').querySelectorAll('img'); //It's an Array of all the images inside '#dealer-box'
 
     for (i=0; i<yourImages.length; i++) {
+      // It's removing all the images at one go
       yourImages[i].remove();
     }
 
     for (i=0; i<dealerImages.length; i++) {
+      // It's removing all the images at one go
       dealerImages[i].remove();
     }
 
+    // Sets the score to 0 when the 'Deal' button is pressed
     YOU['score'] = 0;
     DEALER['score'] = 0;
 
@@ -69,23 +96,27 @@ function blackjackDeal() {
     
     document.querySelector('#your-blackjack-result').style.color = '#ffffff';
     document.querySelector('#dealer-blackjack-result').style.color = '#ffffff';
-
+    
+    // Changing the text content to "Let's Play!" when the 'Deal' button is pressed
     document.querySelector('#blackjack-result').textContent = "Let's Play!";
     document.querySelector('#blackjack-result').style.color = 'black';
-
+    
+    // It's important to set 'turnsOver' to true so that we can show the results
     blackjackGame['turnsOver'] = true;
   }
 }
 
+// This function helps to update score
 function updateScore(card, activePlayer) {
   if (card === 'A') {
-    //If adding 11 keeps me below 21, add 11. Otherwise, add 1.
+    //If adding 11 keeps you below 21, add 11. Otherwise, add 1.
     if(activePlayer['score'] + blackjackGame['cardsMap'][card][1] <= 21) {
-      activePlayer['score'] += blackjackGame['cardsMap'][card][1];
+      activePlayer['score'] += blackjackGame['cardsMap'][card][1];  // Choose the second element from the array (check the key 'cardsMap')
     } else {
-      activePlayer['score'] += blackjackGame['cardsMap'][card][0];
+      activePlayer['score'] += blackjackGame['cardsMap'][card][0];  // Choose the first element from the array (check the key 'cardsMap')
     }
   } else {
+    // If the card is not 'A'
     activePlayer['score'] += blackjackGame['cardsMap'][card];
   }  
 }
@@ -99,11 +130,18 @@ function showScore(activePlayer) {
   }
 }
 
+// Defining sleep function
 function sleep(ms) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+// This function determines what happens when 'Stand' button is pressed
 async function dealerLogic() {
+  /*
+  As soon as the 'Stand' button is pressed,
+  the dealerLogic function runs and it activates the
+  'Stand' mode.
+  */
   blackjackGame['isStand'] = true;
 
   while (DEALER['score'] < 16 && blackjackGame['isStand'] === true) {
@@ -111,7 +149,10 @@ async function dealerLogic() {
     showCard(card, DEALER);
     updateScore(card, DEALER);
     showScore(DEALER);
-    await sleep(1000);
+    
+    // Calling sleep function
+    // Delaying appearance of the random card by 1000ms
+    await sleep(1000); 
   }
   
   blackjackGame['turnsOver'] = true;
@@ -156,6 +197,7 @@ function showResult(winner) {
   let message, messageColor;
 
   if (blackjackGame['turnsOver'] === true) {
+    // Code executes when all turns are over
     if (winner === YOU) {
       document.querySelector('#wins').textContent = blackjackGame['wins'];
       message = 'You Won!';
